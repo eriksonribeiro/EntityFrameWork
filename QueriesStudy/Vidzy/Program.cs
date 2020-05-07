38ed9c4555873f8318fdc2971ad96a97b3331c20
eriksonribeiro <eajribeiro@gmail.com>
2020-05-07 20:43:21 -0300

    Revert "Revert "LINQ Study""
    This reverts commit 8bf7751a8445d30cdeb4dbaeb0ff31bbef4a7704.

---
+++ QueriesStudy/Vidzy/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Remoting.Contexts;
+using Vidzy.Migrations;
+
+namespace Vidzy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var query = new Query();
+            var queryExt = new QueryExtensionMethods();
+
+            query.QueryOne();
+            queryExt.QueryOneDotone();
+
+            query.QueryTwo();
+            queryExt.QueryTwoDotOne();
+
+            query.QueryThree();
+            queryExt.QueryThreeDotOne();
+
+            queryExt.QueryFour();
+            queryExt.QueryFive();
+            queryExt.QuerySix();
+
+        }
+    }
+}
